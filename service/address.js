6d09072a33cf383bfe774df1ce943f1ad578f229
aidanhoppe <68791485+aidanhoppe@users.noreply.github.com>
2022-09-19 04:55:40 -0700

    Update address.js

--- service/address.js
+++ service/address.js
@@ -1,7 +1,7 @@
 const addressDAO = require('../dao/address');
 
 class AddressService {
-  createListing(addressDto) {
+  createAddress(addressDto) {
     const { street_address_1, street_address_2, city, state, zipcode, country } = addressDto;
     return addressDAO.createAddress(
         street_address_1, 
