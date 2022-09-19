090b0452085d2f09ccca3e2aa42f37c1aaa82df2
aidanhoppe <68791485+aidanhoppe@users.noreply.github.com>
2022-09-18 23:14:56 -0700

    cors necessity test

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = require('./routes');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(router);
 
