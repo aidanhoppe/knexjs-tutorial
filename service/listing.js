e1ceca46a63428ce8a14f1cef5194c07bebfe765
aidanhoppe <68791485+aidanhoppe@users.noreply.github.com>
2022-09-19 04:00:52 -0700

    include price+shipping

--- service/listing.js
+++ service/listing.js
@@ -2,10 +2,12 @@ const listingDAO = require('../dao/listing');
 
 class ListingService {
   createListing(listingDto) {
-    const { seller_id, title, description, condition, photos, brand_id, model, category_id, accepting_offers } = listingDto;
+    const { seller_id, title, price, shipping, description, condition, photos, brand_id, model, category_id, accepting_offers } = listingDto;
     return listingDAO.createListing(
         seller_id, 
         title, 
+        price,
+        shipping,
         description,
         condition,
         photos,
