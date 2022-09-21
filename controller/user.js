1da92434599700e74569c8d1e158d98dcc6bde32
aidanhoppe <68791485+aidanhoppe@users.noreply.github.com>
2022-09-20 21:20:07 -0700

    i think its request.params

--- controller/user.js
+++ controller/user.js
@@ -11,7 +11,7 @@ class UserController {
   }
   async getUser(req, res) {
     try {
-        const user = await userService.getUser(req.body)
+        const user = await userService.getUser(req.params)
         res.status(201).json(user)
     } catch (e) {
         console.log(e)
