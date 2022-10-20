1527742565cbb0aa8c5e744c60894c048a9e2b6b
aidanhoppe <68791485+aidanhoppe@users.noreply.github.com>
2022-10-19 21:28:40 -0700

    test again

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-const sslServer = https.createServer(
-    {
-        key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-        cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-    },
-    app
-)
+// const sslServer = https.createServer(
+//     {
+//         key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+//         cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+//     },
+//     app
+// )
 
 // sslServer.listen(8080, () => console.log('server listening on port 8080'));
 app.listen(8080, () => console.log('server listening on port 8080'));
