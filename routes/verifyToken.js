5f5c4c99153eead9972b5aa3acefc497df33bb75
aidanhoppe <68791485+aidanhoppe@users.noreply.github.com>
2022-10-25 20:58:10 -0700

    Update verifyToken.js

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -8,6 +8,7 @@ module.exports = function (req, res, next) {
         // const verified = jwt.verify(token, process.env.TOKEN_SECRET)
         const verified = jwt.verify(token, '7483ae193072664e34e3fd6432a4ad183d132b99c81174989f88db393ec29c5f')
         req.user = verified
+        next()
     } catch (e) {
         console.log(e)
         res.status(400).send('Invalid Token')
