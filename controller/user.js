c1a45210844014d66cfd052bf9bf9cba64cffd04
aidanhoppe <68791485+aidanhoppe@users.noreply.github.com>
2022-10-25 20:20:13 -0700

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -15,6 +15,7 @@ class UserController {
     try {
       const result = await userService.login(req.body)
       if(result?.result == 'Success') {
+        console.log('result: ', result)
         const token = jwt.sign({_id: result.user_id}, '7483ae193072664e34e3fd6432a4ad183d132b99c81174989f88db393ec29c5f')
         req.header('auth-token', token).send(token)
         res.status(200).json(result)
